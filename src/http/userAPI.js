ba5de8180c381a6a8b537b20e6c21dad086dacf7
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-12 17:15:43 +0500

    Update userAPI.js

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,20 +1,29 @@
 import { $host, $authHost } from "./index";
-import { jwtDecode } from "jwt-decode"; // Использование именованного экспорта
+import jwtDecode from "jwt-decode"; // Ensure you're using default export
 
-export const registration = async (email, password) => {
+export const registration = async (email, password, userContext) => {
     const { data } = await $host.post('api/user/registration', { email, password, role: 'USER' });
     localStorage.setItem('token', data.token);
-    return jwtDecode(data.token);
+    const decodedToken = jwtDecode(data.token);
+    userContext.setUser(decodedToken);
+    userContext.setIsAuth(true);
+    return decodedToken;
 }
 
-export const login = async (email, password) => {
+export const login = async (email, password, userContext) => {
     const { data } = await $host.post('api/user/login', { email, password });
     localStorage.setItem('token', data.token);
-    return jwtDecode(data.token);
+    const decodedToken = jwtDecode(data.token);
+    userContext.setUser(decodedToken);
+    userContext.setIsAuth(true);
+    return decodedToken;
 }
 
-export const check = async () => {
+export const check = async (userContext) => {
     const { data } = await $authHost.get('api/user/auth');
     localStorage.setItem('token', data.token);
-    return jwtDecode(data.token);
+    const decodedToken = jwtDecode(data.token);
+    userContext.setUser(decodedToken);
+    userContext.setIsAuth(true);
+    return decodedToken;
 }
