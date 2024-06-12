2f341c8571633442eeef39482047021bc383b0e9
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-12 17:39:44 +0500

    Update UserStore.js

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable } from "mobx";
 
 export default class UserStore {
     constructor() {
@@ -21,9 +21,5 @@ export default class UserStore {
 
     get user() {
         return this._user;
-    }
-
-    get role() {
-        return this._user.role;
     }
 }
