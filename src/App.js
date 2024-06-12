bb6cf08166126635345accb84772bab12123e933
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-12 17:56:51 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {BrowserRouter} from "react-router-dom";
-import AppRouter from "./components/AppRouter";
-import NavBar from "./components/NavBar";
-import {observer} from "mobx-react-lite";
-import {Context} from "./index";
-import {check} from "./http/userAPI";
-import {Spinner} from "react-bootstrap";
+import React, { useContext, useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
+import NavBar from './components/NavBar';
+import { observer } from 'mobx-react-lite';
+import { Context } from './index';
+import { check } from './http/userAPI';
+import { Spinner } from 'react-bootstrap';
+import WebSocketClient from './WebSocketClient'; // Импортируем WebSocketClient
 
 const App = observer(() => {
-    const {user} = useContext(Context)
-    const [loading, setLoading] = useState(true)
+    const { user } = useContext(Context);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Устанавливаем WebSocket-соединение
+        const webSocketClient = new WebSocketClient('ws://5.35.88.8:3000/ws');
+
+        // Обработка завершения установки WebSocket-соединения
+        webSocketClient.onopen = () => {
+            console.log('WebSocket connection established successfully');
+        };
+
+        // Обработка ошибок WebSocket-соединения
+        webSocketClient.onerror = (error) => {
+            console.error('WebSocket connection error:', error);
+        };
+
+        // Отписываемся от WebSocket-соединения при размонтировании компонента
+        return () => {
+            webSocketClient.close();
+        };
+    }, []); // Пустой массив зависимостей, чтобы этот эффект выполнялся только один раз при монтировании
 
     useEffect(() => {
         check().then(data => {
-            user.setUser(true)
-            user.setIsAuth(true)
-        }).finally(() => setLoading(false))
+            user.setUser(true);
+            user.setIsAuth(true);
+        }).finally(() => setLoading(false));
     }, [user]);
 
     if (loading) {
-        return <Spinner animation={"grow"}/>
+        return <Spinner animation="grow" />;
     }
 
     return (
