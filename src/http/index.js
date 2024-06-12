ee6ce547c4c04279104ef74af4f2aa384fa59fd7
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-12 16:09:21 +0500

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,21 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 
 const $host = axios.create({
     baseURL: process.env.REACT_APP_API_URL
-})
+});
 
 const $authHost = axios.create({
     baseURL: process.env.REACT_APP_API_URL
-})
+});
 
 const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    return config
-}
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
+
+$authHost.interceptors.response.use(
+    response => response,
+    async error => {
+        if (error.response && error.response.status === 401) {
+            console.error('Unauthorized access - perhaps the token is invalid or expired');
+        }
+        return Promise.reject(error);
+    }
+);
 
 export {
     $host,
     $authHost
-}
+};
