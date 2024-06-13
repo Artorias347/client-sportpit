2dd7b6886ecc7aac96714df1e1e18f55998e975d
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-13 15:23:27 +0500

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap';
 import CreateBrand from '../components/modals/CreateBrand';
 import CreateDevice from '../components/modals/CreateDevice';
 import CreateType from '../components/modals/CreateType';
-import ManageProducts from '../components/ManageProducts';
+import ManageProducts from '../ManageProducts';
 
 const Admin = () => {
     const [brandVisible, setBrandVisible] = useState(false);
