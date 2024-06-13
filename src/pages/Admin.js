e1f31044a2184f9c63e7728aa7c51f4bc5befaef
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-13 15:30:02 +0500

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import CreateBrand from '../components/modals/CreateBrand';
 import CreateDevice from '../components/modals/CreateDevice';
 import CreateType from '../components/modals/CreateType';
-import ManageProducts from '../components/modals/ManageProducts';
+import { MANAGEPRODUCTS_ROUTE } from '../utils/consts';
 
 const Admin = () => {
     const [brandVisible, setBrandVisible] = useState(false);
     const [typeVisible, setTypeVisible] = useState(false);
     const [deviceVisible, setDeviceVisible] = useState(false);
-    const [manageProductsVisible, setManageProductsVisible] = useState(false);
 
     return (
         <Container className="d-flex flex-column">
@@ -48,19 +48,18 @@ const Admin = () => {
             >
                 Удалить бренд
             </Button>
-            <Button
-                variant="outline-dark"
-                className="mt-4 p-2"
-                onClick={() => setManageProductsVisible(true)}
-            >
-                Управление товаром
-            </Button>
+            <Link to={MANAGEPRODUCTS_ROUTE}>
+                <Button
+                    variant="outline-dark"
+                    className="mt-4 p-2"
+                >
+                    Управление товаром
+                </Button>
+            </Link>
 
             <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)} />
             <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)} />
             <CreateType show={typeVisible} onHide={() => setTypeVisible(false)} />
-            
-            {manageProductsVisible && <ManageProducts />}
         </Container>
     );
 };
