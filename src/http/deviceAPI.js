1e2baf35cb7caf5aa9db66122aea33410e5aae23
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-18 19:39:55 +0500

    Update deviceAPI.js

--- src/http/deviceAPI.js
+++ src/http/deviceAPI.js
@@ -1,34 +1,34 @@
 import {$authHost, $host} from "./index";
-import jwt_decode from "jwt-decode";
 
 export const createType = async (type) => {
-    const {data} = await $authHost.post('api/type', type)
-    return data
-}
+    const { data } = await $authHost.post('api/type', type);
+    return data;
+};
+
 export const deleteType = async (id) => {
     const { data } = await $authHost.delete(`api/type/${id}`);
     return data;
-}
+};
 
 export const fetchTypes = async () => {
-    const {data} = await $host.get('api/type')
-    return data
-}
+    const { data } = await $host.get('api/type');
+    return data;
+};
 
 export const createBrand = async (brand) => {
-    const {data} = await $authHost.post('api/brand', brand)
-    return data
-}
+    const { data } = await $authHost.post('api/brand', brand);
+    return data;
+};
 
 export const fetchBrands = async () => {
-    const {data} = await $host.get('api/brand', )
-    return data
-}
+    const { data } = await $host.get('api/brand');
+    return data;
+};
 
 export const createDevice = async (device) => {
-    const {data} = await $authHost.post('api/device', device)
-    return data
-}
+    const { data } = await $authHost.post('api/device', device);
+    return data;
+};
 
 export const fetchDevices = async (
     typeId, brandId, page, limit, minPrice, maxPrice, inStock, discounted
@@ -40,12 +40,19 @@ export const fetchDevices = async (
     });
     return data;
 };
+
 export const fetchOneDevice = async (id) => {
-    const {data} = await $host.get('api/device/' + id)
-    return data
-}
+    const { data } = await $host.get(`api/device/${id}`);
+    return data;
+};
+
 export const updateDevice = async (id, updatedDevice) => {
     const { data } = await $authHost.put(`api/device/${id}`, updatedDevice);
     return data;
 };
 
+// Реализация функции deleteDevice
+export const deleteDevice = async (id) => {
+    const { data } = await $authHost.delete(`api/device/${id}`);
+    return data;
+};
