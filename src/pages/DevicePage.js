f48e6670862130d7c8cfbcfeeb7f9435982622f5
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-18 22:16:08 +0500

    Update DevicePage.js

--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import { fetchOneDevice } from '../http/deviceAPI';
 import { Context } from '../index';
 import Notification from '../components/Notification';
-import Reviews from '../components/Reviews';
+import Reviews from '../Reviews';
 
 const DevicePage = () => {
   const [device, setDevice] = useState({ info: [] });
