cf15ff99f7376f4e366d9ccc8c28a69f8db85e79
Artorias347 <167741293+Artorias347@users.noreply.github.com>
2024-06-18 16:33:48 +0500

    Update userAPI.js

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,5 +1,5 @@
 import { $host, $authHost } from "./index";
-import { jwtDecode } from "jwt-decode"; // Использование именованного экспорта
+import jwtDecode from "jwt-decode";
 
 export const registration = async (email, password) => {
     const { data } = await $host.post('api/user/registration', { email, password, role: 'USER' });
@@ -17,4 +17,9 @@ export const check = async () => {
     const { data } = await $authHost.get('api/user/auth');
     localStorage.setItem('token', data.token);
     return jwtDecode(data.token);
+}
+
+export const resetPassword = async (email) => {
+    const { data } = await $host.post('api/user/reset-password', { email });
+    return data.message;
 }
